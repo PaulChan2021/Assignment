0a8d79464f197d8c79b619d17d7edc129243c75d
PaulChan2021 <paulchan0728@gmail.com>
2022-01-13 00:10:34 -0800

    Map now can show user's center

--- Assignment/Assignment/ViewController/MapViewController.swift
+++ Assignment/Assignment/ViewController/MapViewController.swift
@@ -13,9 +13,13 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var addressLabel: UILabel!
+    
     let locationManager = CLLocationManager()
     // how big we can see on map
     let regionInMeters: Double = 10000
+    
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,8 +55,7 @@ class MapViewController: UIViewController {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             //showing user location with blue dot
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
+            startTackingUserLocation()
             break
         case .denied:
             // turn on permissions instruction
@@ -65,22 +68,74 @@ class MapViewController: UIViewController {
             //get your location when app is in background
         case .authorizedAlways:
             break
+        @unknown default:
+            break
         }
+        
     }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        //create latitude and longitude based on center
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    func startTackingUserLocation() {
+        // showing user location with blue dot
+        mapView.showsUserLocation = true
+        // user in center
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
 }
 
 extension MapViewController: CLLocationManagerDelegate {
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else {
-            return }
-        let area = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(area, animated: true)
-    }
-    
-    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        // authentication changed and get permission from user again start from line 47
+        // authentication changed and it will ask permissions from user again start from line 47
         checkLocationAuthorization()
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        // get address from lat and long
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else {
+            return }
+        
+        guard center.distance(from: previousLocation) > 50 else {
+            return }
+        
+        self.previousLocation = center
+        // error checking
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else {
+                return }
+            
+            if let _ = error {
+                //TODO: Show alert
+                return
+            }
+            
+            guard let mark = placemarks?.first else {
+                //TODO: Show alert
+                return
+            }
+            // showing street name and number
+            let streetName = mark.thoroughfare ?? ""
+            let streetNumber = mark.subThoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
     }
 }
