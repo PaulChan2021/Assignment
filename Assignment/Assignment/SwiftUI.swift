db00ea770420aff5b70a1a5e1dec745d73f16999
PaulChan2021 <paulchan0728@gmail.com>
2022-01-13 01:40:38 -0800

    building swiftUI
    try to build swiftUI and work with UIkit but didn't work

---
+++ Assignment/Assignment/SwiftUI.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI.swift
+//  Assignment
+//
+//  Created by Banana on 13/1/2022.
+//
+
+import SwiftUI
+
+struct SwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
